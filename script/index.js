5a370b3a523de8445334897d638d0924fd5e92c5
maoritz <maorattias17@gmail.com>
2021-02-18 23:12:25 +0200

    sticky navigation , mobile menu

---
+++ script/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    // For the sticky nav-bar
+    $(".js--programs-section").waypoint(
+      function (direction) {
+        if (direction == "down") {
+          $("nav").addClass("sticky");
+        } else {
+          $("nav").removeClass("sticky");
+        }
+      },
+      { offset: "16%" }
+    );
+
+    // For the mobile menu
+    $('.js--nav-icon').click(function(){
+        const nav = $('.js--main-nav');
+        nav.slideToggle(200)
+    });
+
+})
+
