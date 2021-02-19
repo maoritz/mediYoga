d830f6b07a8aee35b97ccae5001132d00ce2a87c
maoritz <maorattias17@gmail.com>
2021-02-19 17:33:39 +0200

    mobile navigation menu

--- script/index.js
+++ script/index.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
-    // For the sticky nav-bar
-    $(".js--programs-section").waypoint(
-      function (direction) {
-        if (direction == "down") {
-          $("nav").addClass("sticky");
-        } else {
-          $("nav").removeClass("sticky");
-        }
-      },
-      { offset: "16%" }
-    );
+  // For the sticky nav-bar
+  $(".js--programs-section").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    { offset: "22%" }
+  );
 
-    // For the mobile menu
-    $('.js--nav-icon').click(function(){
-        const nav = $('.js--main-nav');
-        nav.slideToggle(200)
-    });
+  // For the mobile menu
+  $('.js--nav-icon').click(function(){
+      const nav = $('.js--main-nav');
+      nav.slideToggle(200)
+  });
 
 })
-
