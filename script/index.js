537a7419855c3a4a7038f434a01636d1782e0a51
maoritz <maorattias17@gmail.com>
2021-02-21 02:34:20 +0200

    buttons scroling to element, effects when elements show up , relevant main nav

--- script/index.js
+++ script/index.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
         $("nav").addClass("sticky");
         $('nav').css('padding-top','0')
       } else {
+        $('nav').css('padding-top','3rem')
         $("nav").removeClass("sticky");
       }
     },
@@ -17,5 +18,62 @@ $(document).ready(function () {
       const nav = $('.js--main-nav');
       nav.slideToggle(200)
   });
+
+  // Scroll on buttons
+  $(".js--scroll-to-header").click(function(){
+    $('html,body').animate({scrollTop:  $('.js--header').offset().top -100},1000)
+  })
+
+  $(".js--scroll-to-about-us").click(function(){
+    $('html,body').animate({scrollTop:  $('.js--about-us-section').offset().top -100},1000)
+  })
+  
+  $(".js--scroll-to-programs").click(function(){
+    $('html,body').animate({scrollTop:  $('.js--programs-section ').offset().top -100},1000)
+  })
+
+  $(".js--scroll-to-schedule").click(function(){
+    $('html,body').animate({scrollTop:  $('.js--Schedule-section').offset().top -100},1000)
+  })
+  
+  $(".js--scroll-to-team").click(function(){
+    $('html,body').animate({scrollTop:  $('.js--team-section').offset().top -100},1000)
+  })
+
+  $(".js--scroll-to-pricing").click(function(){
+    $('html,body').animate({scrollTop:  $('.js--pricing-section').offset().top -100},1000)
+  })
+
+
+  // Animations on scroll
+  $('.js--wp-1').waypoint(function(direction){
+    $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+  },{
+    offset: '60%'
+  })
+
+  $('.js--wp-2').waypoint(function(direction){
+    $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+  },{
+    offset: '70%'
+  })
+
+  $('.js--wp-3').waypoint(function(direction){
+    $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+  },{
+    offset: '60%'
+  })
+
+  $('.js--wp-4').waypoint(function(direction){
+    $('.js--wp-4').addClass('animate__animated animate__headShake');
+  },{
+    offset: '70%'
+  })
+
+  $('.js--wp-5').waypoint(function(direction){
+    $('.js--wp-5').addClass('animate__animated animate__fadeIn');
+  },{
+    offset: '65%'
+  })
 
 })
